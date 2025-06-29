c557891f61c52a7c4eb9d12522cdd6f94efc38fe
naveennk045 <naveennk045@gmail.com>
2025-06-29 17:01:28 +0530

    Setting up mockito testing

---
+++ week2/Mocito/src/main/java/com/mockito/Mocito/MocitoApplication.java
@@ -0,0 +1,13 @@
+package com.mockito.Mocito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocitoApplication.class, args);
+	}
+
+}
