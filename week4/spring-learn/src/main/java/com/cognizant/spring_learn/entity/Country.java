6408f70185ad78f5169644217a0bb84ad43e24d5
Naveenkumar <naveennk045@gmail.com>
2025-07-13 19:10:27 +0530

    implementation of JWT Authentication for RESTful Web Service using Spring Securit

--- week4/spring-learn/src/main/java/com/cognizant/spring_learn/entity/Country.java
+++ week4/spring-learn/src/main/java/com/cognizant/spring_learn/entity/Country.java
@@ -1,10 +1,12 @@
 package com.cognizant.spring_learn.entity;
 
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
 @Component
 @Scope("singleton")
 public class Country {
